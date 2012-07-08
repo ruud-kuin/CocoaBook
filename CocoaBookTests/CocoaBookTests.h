0b50601f0b3d74ca1b718ee55d53aca9ff460016
Ruud Kuin <ruudkuin@MacBook-Pro-van-Ruud-2.local>
2012-07-08 13:38:27 +0200

    Initial Commit

---
+++ CocoaBookTests/CocoaBookTests.h
@@ -0,0 +1,13 @@
+//
+//  CocoaBookTests.h
+//  CocoaBookTests
+//
+//  Created by Ruud Kuin on 08-07-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CocoaBookTests : SenTestCase
+
+@end
---
+++ CocoaBookTests/CocoaBookTests.m
@@ -0,0 +1,32 @@
+//
+//  CocoaBookTests.m
+//  CocoaBookTests
+//
+//  Created by Ruud Kuin on 08-07-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CocoaBookTests.h"
+
+@implementation CocoaBookTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CocoaBookTests");
+}
+
+@end
